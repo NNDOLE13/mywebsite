50998b62442e14c83aba06bdb481635d755ab6fe
NNDOLE13 <153237652+NNDOLE13@users.noreply.github.com>
2024-02-29 22:58:10 +0000

    Thu, Feb 29, 2024, 2:58 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,18 @@
 loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
 
-load = 90
+load = 0
 
-loadText.innerText = ${load}%
+setInterval(blurring,50)
+
+function blurring() {
+    load++
+
+    if (load > 99){
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 1
+    bg.style.filter = "blur(10px)"
+}
 
